363deeaf50abc7a34f834206e915dd88243a463b
Suchit Gupta <suchitgupta027@gmail.com>
2022-10-25 10:16:22 +0530

    lab questions

--- lab5.c
+++ lab5.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 void question1()
 {
-    int s[100], n, max = s[0], min = s[0];
-    printf("Enter the size of the array");
+    int s[100], n;
+    printf("Enter the size of the array\n");
     scanf("%d", &n);
-    printf("Enter the array");
+    printf("Enter the array\n");
     for (int i = 0; i < n; ++i)
     {
         scanf("%d", &s[i]);
     }
+    int max = s[0], min = s[0];
     for (int i = 0; i < n; ++i)
     {
         if (s[i] > max)
@@ -22,7 +23,61 @@ void question1()
     }
     printf("max is %d min is %d", max, min);
 }
+void question2()
+{
+    int s[100], n;
+    char order;
+    printf("Enter a for ascending and d for descending order\n");
+    scanf("%c", &order);
+    printf("Enter the size of the array\n");
+    scanf("%d", &n);
+    printf("Enter the array\n");
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", &s[i]);
+    }
+    switch (order)
+    {
+    case 'a':
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i; j < n; ++j)
+            {
+                int temp;
+                if (s[i] > s[j])
+                {
+                    temp = s[i];
+                    s[i] = s[j];
+                    s[j] = temp;
+                }
+            }
+        }
+        break;
+    case 'd':
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i; j < n; ++j)
+            {
+                int temp;
+                if (s[i] < s[j])
+                {
+                    temp = s[i];
+                    s[i] = s[j];
+                    s[j] = temp;
+                }
+            }
+        }
+        break;
+    default:
+        printf("error");
+        break;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%d", s[i]);
+    }
+}
 int main()
 {
-    question1();
+    question2();
 }
