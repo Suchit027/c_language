475a2db5e3eb6754c53f6f1d0a700a02779d208a
Suchit Gupta <suchitgupta027@gmail.com>
2022-10-28 19:52:55 +0530

    array practise

--- 5.c
+++ 5.c
@@ -86,6 +86,7 @@ void sortdescending()
         printf("%d\t", s[i]);
     }
 }
+<<<<<<< HEAD
 void bubbleshort(){
     int s[100], n;
     printf("Enter the order");
@@ -108,6 +109,60 @@ void bubbleshort(){
     }
     for(int i = 0; i < n ; ++i)
     printf("%d", s[i]);
+=======
+void selectionsort(){
+     int s[100], n;
+    char order  = '\0' ;
+    printf("Enter the size of the array\n");
+    scanf("%d", &n);
+    printf("Enter a for ascending and d for descending order\n");
+    scanf(" %c", &order);
+    printf("Enter the array\n");
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", &s[i]);
+    }
+    switch (order)
+    {
+    case 'a':
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i; j < n; ++j)
+            {
+                int temp;
+                if (s[i] > s[j])
+                {
+                    temp = s[i];
+                    s[i] = s[j];
+                    s[j] = temp;
+                }
+            }
+        }
+        break;
+    case 'd':
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i; j < n; ++j)
+            {
+                int temp;
+                if (s[i] < s[j])
+                {
+                    temp = s[i];
+                    s[i] = s[j];
+                    s[j] = temp;
+                }
+            }
+        }
+        break;
+    default:
+        printf("error");
+        break;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%d", s[i]);
+    }
+>>>>>>> 7550cb4 (array practise)
 }
 int main()
 {
