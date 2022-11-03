28124b1d1394ec075371a65f36bc6a6755b6db86
Suchit Gupta <suchitgupta027@gmail.com>
2022-11-03 09:32:17 +0530

    lab questions

--- lab5.c
+++ lab5.c
@@ -39,7 +39,7 @@ void question2()
     switch (order)
     {
     case 'd':
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n-1; ++i)
         {
             for (int j = 0; j < (n - i - 1); ++j)
             {
@@ -54,7 +54,7 @@ void question2()
         }
         break;
     case 'a':
-        for (int j = 0; j < n; ++j)
+        for (int j = 0; j < n-1; ++j)
         {
             for (int i = 0; i < (n - j - 1); ++i)
             {
